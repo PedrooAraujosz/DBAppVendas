4946f68a3bedd4e14b762a59286d62d644bcd935
PedrooAraujosz <mxpedro.araujo11@gmail.com>
2024-09-25 11:53:53 -0300

    Finalizado a view index de Produto

--- AppVendas/Models/Produto.cs
+++ AppVendas/Models/Produto.cs
@@ -1,14 +1,29 @@
-﻿namespace AppVendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVendas.Models
 {
     public class Produto
     {
         public Guid ProdutoId { get; set; }
+
+        [Display(Name = "descrição")]
+        [Required(ErrorMessage = "O campo é obrigatorio")]
+        [MaxLength(100, ErrorMessage = "A descrição deve ter, no maximo, 100 caracteres! ")]
         public string ProdutoNome { get; set; }
+
+        [Required(ErrorMessage = "O campo não pode ser vazio!")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor do produto deve ser um número positivo!")]
+
         public double Valor { get; set; }
+        [Required(ErrorMessage = "O campo não pode ser vazio!")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor do produto deve ser um número positivo!")]
+        [Display(Name = "Estoque atual")]
         public double QtadeEstoque { get; set; }
-        public bool CadastroAtivo { get; set; }
+        [Display(Name = "Positivo?")]
+        public bool CadastroAtivo { get; set; } = true;
 
         /* Chave Estrangeira */
+        [Required(ErrorMessage = " Por favor, selecione uma Cliente")]
         public Guid CategoriaId { get; set; }
         public Categoria? Categoria { get; set; }
     }
