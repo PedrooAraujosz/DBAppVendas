09ce0110c50a0c93f97d8d03cfdbe665756d96d4
PedrooAraujosz <mxpedro.araujo11@gmail.com>
2024-09-25 10:11:21 -0300

    Adicionar arquivos de projeto.

---
+++ AppVendas/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVendas.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+
+        [Display(Name = "descrição")]
+        [Required(ErrorMessage = "O campo é obrigatorio")]
+        [MaxLength(100, ErrorMessage = "A descrição deve ter, no maximo, 100 caracteres! ")]
+        public string CategoriaNome { get; set; }
+    }
+}
